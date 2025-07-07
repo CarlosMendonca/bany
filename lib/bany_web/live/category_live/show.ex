5bfa4f16ae3a567d8c35b5cb7c0721124a4e044b
Carlos Mendonça <CarlosMendonca@users.noreply.github.com>
2025-07-07 02:06:41 -0400

    Generating Category entity

---
+++ lib/bany_web/live/category_live/show.ex
@@ -0,0 +1,37 @@
+defmodule BanyWeb.CategoryLive.Show do
+  use BanyWeb, :live_view
+
+  alias Bany.Budget
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <Layouts.app flash={@flash}>
+      <.header>
+        Category {@category.id}
+        <:subtitle>This is a category record from your database.</:subtitle>
+        <:actions>
+          <.button navigate={~p"/categories"}>
+            <.icon name="hero-arrow-left" />
+          </.button>
+          <.button variant="primary" navigate={~p"/categories/#{@category}/edit?return_to=show"}>
+            <.icon name="hero-pencil-square" /> Edit category
+          </.button>
+        </:actions>
+      </.header>
+
+      <.list>
+        <:item title="Name">{@category.name}</:item>
+      </.list>
+    </Layouts.app>
+    """
+  end
+
+  @impl true
+  def mount(%{"id" => id}, _session, socket) do
+    {:ok,
+     socket
+     |> assign(:page_title, "Show Category")
+     |> assign(:category, Budget.get_category!(id))}
+  end
+end
