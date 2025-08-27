65a4d2236b66a91c39e48b6280722b06ab37ce3e
Carlos Mendonça <CarlosMendonca@users.noreply.github.com>
2025-08-27 01:39:36 -0400

    Adding month navigation

--- lib/bany_web/live/category_live/index_with_totals.ex
+++ lib/bany_web/live/category_live/index_with_totals.ex
@@ -8,8 +8,20 @@ defmodule BanyWeb.CategoryLive.IndexWithTotals do
     ~H"""
     <Layouts.app flash={@flash}>
       <.header>
-        Listing Categories with Totals
+        Categories with Totals for <%= Calendar.strftime(@selected_month, "%B %Y") %>
       </.header>
+
+      <div class="flex justify-between items-center mb-4">
+        <.link navigate={~p"/categories/with_totals/#{@previous_month.year}/#{@previous_month.month}"}>
+          <.button>Previous Month</.button>
+        </.link>
+        <.link navigate={~p"/categories/with_totals/#{@current_month.year}/#{@current_month.month}"}>
+          <.button>Today</.button>
+        </.link>
+        <.link navigate={~p"/categories/with_totals/#{@next_month.year}/#{@next_month.month}"}>
+          <.button>Next Month</.button>
+        </.link>
+      </div>
 
       <%= for {group, categories} <- @category_groups do %>
         <div class="mb-8">
@@ -38,11 +50,19 @@ defmodule BanyWeb.CategoryLive.IndexWithTotals do
   @impl true
   def mount(params, _session, socket) do
     {year, month} = get_year_and_month(params)
+    selected_month = Date.new!(year, month, 1)
+
+    previous_month = Date.shift(selected_month, day: -1)
+    next_month = Date.shift(selected_month, month: 1)
 
     socket =
       socket
       |> assign(:page_title, "Listing Categories with Totals")
       |> assign(:category_groups, Budget.list_categories_with_totals(month, year))
+      |> assign(:selected_month, selected_month)
+      |> assign(:current_month, Date.utc_today())
+      |> assign(:previous_month, previous_month)
+      |> assign(:next_month, next_month)
 
     {:ok, socket}
   end
